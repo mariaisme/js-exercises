0b01e42a31e775fd9d2644926d9332c6da0b8588
mariaisme <13594936+mariaisme@users.noreply.github.com>
2025-05-06 19:58:50 +0800

    Edited folder name

---
+++ 01-variables-and-operators/javascript.js
@@ -0,0 +1,17 @@
+let name = "John";      // let instantiates the variable
+let surname = "Doe";
+
+console.log(name);      // outputs the name instantiated
+console.log(surname);
+
+let age = 11;
+console.log(age);       // outputs 11 to the console
+age = 54;
+console.log(age);       // since age was reassigned, the console shows 54
+
+const pi = 3.14;        // const cannot be reassigned, attempting to do so will show an error
+console.log(pi);
+
+console.log("23+97="+ (23 + 97));
+console.log(1+2+3+4+5+6);
+console.log((4 + 6 + 9) / 77);
